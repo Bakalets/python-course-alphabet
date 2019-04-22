92a5cba90591579c1900de56ad13c8e816ba7f86
Bakalets <besiki78@gmail.com>
2019-04-22 22:43:44 +0300

    change task7

--- build_in_methods_iterators/homework.py
+++ build_in_methods_iterators/homework.py
@@ -75,7 +75,8 @@ def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
     """
     Find max value from list of lists
     """
-    return max(max(map(lambda x: x, data)))
+    #return max(max(map(lambda x: x, data)))
+    return max(map(lambda x: max(x) if x else 0, data))
 
 
 def task_8_sum_of_ints(data: List[int]) -> int:
